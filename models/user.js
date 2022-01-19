a6e91e7956e26de870378f488b37e0895803f5b5
dev-vicki <vignesh.r1999@gmail.com>
2022-01-19 13:34:24 +0530

    defined timestamp in the schema

--- models/user.js
+++ models/user.js
@@ -2,50 +2,52 @@ const mongoose = require("mongoose");
 const crypto = require("crypto");
 const uuidv1 = require("uuid/v1");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      maxlength: 32,
+      trim: true,
+    },
 
-        name: {
-            type: String,
-            required: true,
-            maxlength: 32,
-            trim: true
-        },
+    lastname: {
+      type: String,
+      maxlength: 32,
+      trim: true,
+    },
 
-        lastname: {
-            type: String,
-            maxlength: 32,
-            trim: true
-        },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
 
-        email: {
-            type: String,
-            trim: true,
-            required: true,
-            unique: true
-        },
+    userinfo: {
+      type: String,
+      trim: true,
+    },
 
-        userinfo:{
-            type:String,
-            trim: true
-        },
+    encry_password: {
+      type: String,
+      required: true,
+    },
 
-        encry_password:{
-            type: String,
-            required: true
-        },
+    salt: String,
 
-        salt: String,
+    role: {
+      type: Number,
+      default: 0,
+    },
 
-        role: {
-            type:Number,
-            default: 0
-        },
-
-        purchases: {
-            type: Array,
-            default: []
-        }
-});
+    purchases: {
+      type: Array,
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
 
 
 // creating method in mongoose
