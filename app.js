58744363498d316209222e727a718cbc8d03c9a6
dev-vicki <vignesh.r1999@gmail.com>
2022-01-20 16:44:06 +0530

    used middlewares

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 mongoose.connect(process.env.DATABASE, {
     useNewUrlParser: true,
@@ -10,6 +13,11 @@ mongoose.connect(process.env.DATABASE, {
 }).then(() => {
     console.log("DB CONNECTED");
 })
+
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
 
 const port = process.env.PORT || 8000;
 
