74d04e41ebcb57e6e737d7cac536560e813689e9
dev-vicki <vignesh.r1999@gmail.com>
2022-01-25 13:44:20 +0530

    created category section with param

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const cors = require("cors");
 // Getting My Routes
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
+
 
 // DB Connection
 mongoose.connect(process.env.DATABASE, {
@@ -28,6 +30,8 @@ app.use(cors());
 // Routes
 app.use("/api", authRoutes);
 app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
+
 
 // PORT
 const port = process.env.PORT || 8000;
