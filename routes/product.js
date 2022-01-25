939f32a76778e7f5d043f19216dd571a4575cdb3
dev-vicki <vignesh.r1999@gmail.com>
2022-01-25 16:52:08 +0530

    getting products with id

---
+++ routes/product.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+const {getProductById} = require("../controllers/product");
+const {isSignedIn, isAuthenticated, isAdmin} = require("../controllers/auth");
+const {getUserById} = require("../controllers/user");
+const { route } = require("./auth");
+
+// all of params
+router.param("userId", getUserById);
+router.param("productId", getProductById);
+
+// all of actual routes
+
+
+module.exports = router;
