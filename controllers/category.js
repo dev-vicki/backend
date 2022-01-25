37017effbaff3f99e3210209fa43560c7bbbaec7
dev-vicki <vignesh.r1999@gmail.com>
2022-01-25 13:59:47 +0530

    get single and multiple categories

--- controllers/category.js
+++ controllers/category.js
@@ -24,3 +24,18 @@ exports.createCategory = (req, res) => {
         res.json({ category });
     })
 };
+
+exports.getCategory = (req, res) => {
+    return res.json(req.category);
+}
+
+exports.getAllCategory = (req, res) => {
+    Category.find().exec((err, categories) => {
+        if (err) {
+          return res.status(400).json({
+            error: "No categories found in DB",
+          });
+        }
+        res.json(categories);
+    })
+};
--- routes/category.js
+++ routes/category.js
@@ -1,7 +1,12 @@
 const express = require("express")
 const router = express.Router()
 
-const { getCategoryById, createCategory } = require("../controllers/category");
+const {
+  getCategoryById,
+  createCategory,
+  getCategory,
+  getAllCategory,
+} = require("../controllers/category");
 const {isSignedIn, isAuthenticated, isAdmin} = require("../controllers/auth");
 const {getUserById} = require("../controllers/user");
 
@@ -12,5 +17,9 @@ router.param("categoryId", getCategoryById);
 
 // Actual routes goes here
 router.post("/category/create/:userId", isSignedIn, isAuthenticated, isAdmin, createCategory)
+
+router.get("/category/:categoryId", getCategory)
+router.get("/categories", getAllCategory);
+
 
 module.exports = router;
