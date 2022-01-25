6b6f412e8608523a0969ad539b4c36ebbb5a1b86
dev-vicki <vignesh.r1999@gmail.com>
2022-01-25 18:25:19 +0530

    updation and deletion of product.

--- routes/product.js
+++ routes/product.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const {getProductById, createProduct, getProduct, photo} = require("../controllers/product");
+const {
+  getProductById,
+  createProduct,
+  getProduct,
+  photo,
+  deleteProduct,
+  updateProduct
+} = require("../controllers/product");
 const {isSignedIn, isAuthenticated, isAdmin} = require("../controllers/auth");
 const {getUserById} = require("../controllers/user");
 const { route } = require("./auth");
@@ -11,9 +18,25 @@ router.param("userId", getUserById);
 router.param("productId", getProductById);
 
 // all of actual routes
+// create routes
 router.post("/product/create/:userId", isSignedIn, isAuthenticated, isAdmin, createProduct);
 
+// read routes
 router.get("/product/:productId", getProduct)
 router.get("/product/photo/:productId", photo);
+
+// delete route
+router.delete("/product/:productId/:userId", isSignedIn,isAuthenticated,isAdmin, deleteProduct);
+
+// update route
+router.put(
+  "/product/:productId/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  updateProduct
+);
+
+// listing route
 
 module.exports = router;
