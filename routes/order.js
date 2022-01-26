8109a7e4b0d0cc98ac6f3e68268d061f8fbcf065
dev-vicki <vignesh.r1999@gmail.com>
2022-01-26 12:30:22 +0530

    create order page

--- routes/order.js
+++ routes/order.js
@@ -4,13 +4,15 @@ const router = express.Router();
 const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth");
 const { getUserById, pushOrderInPurchaseList } = require("../controllers/user");
 const {updateStock} = require("../controllers/product");
-const {getOrderById} = require("../controllers/order");
+const {getOrderById, createOrder} = require("../controllers/order");
 
 // params
 router.param("userId", getUserById);
 router.param("orderId", getOrderById);
 
 // actual routes
-
+// create
+router.post("/order/create/:userId", isSignedIn, isAuthenticated, pushOrderInPurchaseList, updateStock, createOrder);
+// read
 
 module.exports = router;
